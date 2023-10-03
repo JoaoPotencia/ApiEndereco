1b5461a0e8b1a24b593c08fab9a5fc86c687d2e6
João Vitor Santos Raimondi <124799423+JoaoPotencia@users.noreply.github.com>
2023-10-02 22:44:29 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,20 @@ import mongoose from 'mongoose';
 
 import { router } from './router';
 
-// realiza a conexão com o banco de dados
+// realiza a conexão com o mongodb
 mongoose.connect('mongodb://localhost:27017')
 	.then(() => {
-		// tenta executar este codigo
+		
 		const app = express();
 		const port = 3000;
 
 		app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 		app.use(express.json());
 		app.use(router);
-		//teclado de emotion tecla win+.
+		
 		app.listen(port, () => {
 			console.log(`🚗Server is runing on http://localhost:${port}`);
 		});
 	})
-	// caso ocorra algum erro, exibe a mensagem de erro
+	
 	.catch(() => console.log('Erro ao conectar no mongoDb'));
